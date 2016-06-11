4f31784a36b2461b21ab6b422519dec6de2e1da9
Scott Anderson <andersco@gmail.com>
2016-06-10 21:11:33 -0700

    added angular routing

---
+++ src/FreedomCalculator2/wwwroot/scripts/app.js
@@ -0,0 +1,24 @@
+﻿var freedomCalculatorApp = angular.module('freedomCalculatorApp', ['ngRoute']);
+
+// configure our routes
+freedomCalculatorApp.config(function ($routeProvider) {
+	$routeProvider
+		// route for the home page
+		.when('/', {
+			templateUrl: 'templates/home.html',
+			controller: 'mainController'
+		})
+		// route for the login page
+		.when('/login', {
+			templateUrl: 'templates/login.html',
+			controller: 'loginController'
+		});
+});
+
+freedomCalculatorApp.controller('mainController', function ($scope) {
+	$scope.message = 'from the main controller!';
+});
+
+freedomCalculatorApp.controller('loginController', function ($scope) {
+	$scope.message = 'from the login controller!';
+});
