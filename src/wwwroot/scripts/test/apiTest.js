b3902477ca1028a3020a1ba7412e96923099810d
Scott Anderson <andersco@gmail.com>
2017-03-24 21:10:12 -0700

    progress adding budget

--- src/wwwroot/scripts/test/apiTest.js
+++ src/wwwroot/scripts/test/apiTest.js
@@ -1,5 +1,11 @@
 var api = require('../src/api');
 var assert = require('assert');
+var sinon = require('sinon');
+var sinonTest = require('sinon-test');
+
+sinon.test = sinonTest.configureTest(sinon);
+sinon.testCase = sinonTest.configureTestCase(sinon);
+
 describe('api', function() {
     it('should have a getToken() Function', function() {
         assert.ok(typeof api.default.getToken == 'function', 'getToken function not found');
@@ -7,5 +13,11 @@ describe('api', function() {
 
     it('should have an addBudget() function', function() {
         assert.ok(typeof api.default.addBudget == 'function', 'addBudget function not found');
+    });
+
+    describe('addBudget', function() {
+        it('should get the added budget object with an id', sinon.test(function() {
+            //assert.ok(true, true);
+        }));
     });
 });
--- test/BudgetsControllerTest.cs
+++ test/BudgetsControllerTest.cs
@@ -1,9 +1,9 @@
 using FreedomCalculator2.Controllers;
 using FreedomCalculator2.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
@@ -12,41 +12,25 @@ namespace FreedomCalculator2.Tests
 {
     public class BudgetsControllerTests
     {
-    //     [Fact]
-    //     public async Task Post_CreatesBudget()
-    //     {
-    //         // Arrange
-    //         var mockRepo = new Mock<IFreedomCalculatorRepository>();
-    //         mockRepo.Setup(repo => repo.ListAsync()).Returns(Task.FromResult(GetTestSessions()));
-    //         var controller = new HomeController(mockRepo.Object);
+        [Fact]
+        public async Task Post_CreatesBudget()
+        {
+            // Arrange
+            var mockUserStore = new Mock<IUserStore<ApplicationUser>>();
+            var userManager = new UserManager<ApplicationUser>(mockUserStore.Object, null, null, null, null, null, null, null, null);
+            var mockRepo = new Mock<IFreedomCalculatorRepository>();
+            Budget newBudget = new Budget();
+            mockRepo.Setup(repo => repo.AddBudget(newBudget)).Returns(Task.FromResult(1));
+            ClaimsPrincipal user = new ClaimsPrincipal(new ClaimsIdentity());
 
-    //         // Act
-    //         var result = await controller.Index();
+            var controller = new BudgetController(userManager, mockRepo.Object);
+            controller.ControllerContext = new ControllerContext() { HttpContext = new DefaultHttpContext() { User = user} };
 
-    //         // Assert
-    //         var viewResult = Assert.IsType<ViewResult>(result);
-    //         var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
-    //             viewResult.ViewData.Model);
-    //         Assert.Equal(2, model.Count());
-    //     }
-    //     }
+            // Act
+            Budget result = await controller.Post(newBudget);
 
-    //     private List<BrainstormSession> GetTestSessions()
-    //     {
-    //         var sessions = new List<BrainstormSession>();
-    //         sessions.Add(new BrainstormSession()
-    //         {
-    //             DateCreated = new DateTime(2016, 7, 2),
-    //             Id = 1,
-    //             Name = "Test One"
-    //         });
-    //         sessions.Add(new BrainstormSession()
-    //         {
-    //             DateCreated = new DateTime(2016, 7, 1),
-    //             Id = 2,
-    //             Name = "Test Two"
-    //         });
-    //         return sessions;
-    //     }
-    // }
+            // Assert
+            Assert.Equal(1, result.BudgetId);
+        }
+    }
 }
