06c8ef7b938f1e1accb29029dd9110622db76745
Scott Anderson <andersco@gmail.com>
2018-01-10 17:51:44 -0800

    Add EF migration for openiddict changes

---
+++ src/Migrations/20180111013933_openiddict-changes.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FreedomCalculator2.Migrations
+{
+    public partial class openiddictchanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictApplications_ApplicationId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OpenIddictTokens_Hash",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Ciphertext",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Payload",
+                table: "OpenIddictTokens",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReferenceId",
+                table: "OpenIddictTokens",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictTokens_ReferenceId",
+                table: "OpenIddictTokens",
+                column: "ReferenceId",
+                unique: true,
+                filter: "[ReferenceId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictApplications_ApplicationId",
+                table: "OpenIddictTokens",
+                column: "ApplicationId",
+                principalTable: "OpenIddictApplications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId",
+                table: "OpenIddictTokens",
+                column: "AuthorizationId",
+                principalTable: "OpenIddictAuthorizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictApplications_ApplicationId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OpenIddictTokens_ReferenceId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Payload",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "ReferenceId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ciphertext",
+                table: "OpenIddictTokens",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hash",
+                table: "OpenIddictTokens",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictTokens_Hash",
+                table: "OpenIddictTokens",
+                column: "Hash",
+                unique: true,
+                filter: "[Hash] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictApplications_ApplicationId",
+                table: "OpenIddictTokens",
+                column: "ApplicationId",
+                principalTable: "OpenIddictApplications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId",
+                table: "OpenIddictTokens",
+                column: "AuthorizationId",
+                principalTable: "OpenIddictAuthorizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
