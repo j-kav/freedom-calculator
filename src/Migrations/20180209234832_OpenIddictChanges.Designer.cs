98d933ee038e24c7337d66a03b01e52aa99e68e8
Scott Anderson <andersco@gmail.com>
2018-02-09 15:53:36 -0800

    Add openiddict migration

--- src/Migrations/20180209234832_OpenIddictChanges.Designer.cs
+++ src/Migrations/20180209234832_OpenIddictChanges.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FreedomCalculator2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180209234832_OpenIddictChanges")]
+    partial class OpenIddictChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
