9980a9f5fe9011369ebc2db23f0478379ccf938c
Scott Anderson <andersco@gmail.com>
2018-04-03 18:08:55 -0700

    convert promises to async

--- src/wwwroot/scripts/src/components/Statistics.vue
+++ src/wwwroot/scripts/src/components/Statistics.vue
@@ -63,8 +63,7 @@
                     </div>
                     <div class="content-width-container">
                         <div class="content-width-container-header">Total Income</div>
-                        <div class="align-right">{{ utils.usdFormatter.format(this.$store.getters.averageEarnedIncome + this.$store.getters.averagePassiveIncome)
-                            }}
+                        <div class="align-right">{{ utils.usdFormatter.format(this.$store.getters.averageEarnedIncome + this.$store.getters.averagePassiveIncome) }}
                         </div>
                     </div>
                     <div class="content-width-container">
@@ -147,225 +146,228 @@
 </template>
 
 <script>
-    import api from '../api'
-    import utils from '../utils'
-    import Modal from './Modal.vue'
-    import ExpenseAverages from './ExpenseAverages.vue'
-    import Loading from './Loading.vue'
-    import Chart from 'chart.js'
-    import NetWorthChart from './NetWorthChart.vue'
-    import AssetBreakdown from './AssetBreakdown.vue'
+import api from '../api'
+import utils from '../utils'
+import Modal from './Modal.vue'
+import ExpenseAverages from './ExpenseAverages.vue'
+import Loading from './Loading.vue'
+import Chart from 'chart.js'
+import NetWorthChart from './NetWorthChart.vue'
+import AssetBreakdown from './AssetBreakdown.vue'
 
-    export default {
-        name: 'Statistics',
-        components: {
-            'modal': Modal,
-            'loading': Loading,
-            'expenseAverages': ExpenseAverages,
-            'netWorthChart': NetWorthChart,
-            'assetBreakdown': AssetBreakdown
-        },
-        created() {
-            // if there are no assets in the store, get all the data and then create charts
-            if (!this.$store.state.assets) {
-                this.getData().then(() => {
-                    this.createNetWorthBarChart()
-                    this.createIndependenceEstimatePieChart()
-                }).catch((error) => {
-                    console.log('Error getting statistics: ' + error)
-                })
-            }
-        },
-        mounted() {
-            // if there is already data in the store, just create charts from it
-            if (this.$store.state.assets) {
+export default {
+    name: 'Statistics',
+    components: {
+        modal: Modal,
+        loading: Loading,
+        expenseAverages: ExpenseAverages,
+        netWorthChart: NetWorthChart,
+        assetBreakdown: AssetBreakdown
+    },
+    async created() {
+        // if there are no assets in the store, get all the data and then create charts
+        if (!this.$store.state.assets) {
+            try {
+                await this.getData()
                 this.createNetWorthBarChart()
                 this.createIndependenceEstimatePieChart()
+                this.loading = false
+            } catch (error) {
+                this.loading = false
+                this.error = error
             }
+        }
+    },
+    mounted() {
+        // if there is already data in the store, just create charts from it
+        if (this.$store.state.assets) {
+            this.createNetWorthBarChart()
+            this.createIndependenceEstimatePieChart()
+        }
+    },
+    data() {
+        return {
+            loading: !this.$store.state.assets,
+            error: null,
+            showAssetBreakdown: false,
+            showNetWorth: false,
+            showAverageMandatoryExpenses: false,
+            showAverageDiscretionaryExpenses: false,
+            utils: utils
+        }
+    },
+    computed: {
+        totalAssets() {
+            return utils.usdFormatter.format(this.$store.getters.totalAssets)
         },
-        data() {
-            return {
-                loading: !this.$store.state.assets,
-                error: null,
-                showAssetBreakdown: false,
-                showNetWorth: false,
-                showAverageMandatoryExpenses: false,
-                showAverageDiscretionaryExpenses: false,
-                utils: utils
-            }
+        totalLiabilities() {
+            return utils.usdFormatter.format(this.$store.getters.totalLiabilities)
         },
-        computed: {
-            totalAssets() {
-                return utils.usdFormatter.format(this.$store.getters.totalAssets)
-            },
-            totalLiabilities() {
-                return utils.usdFormatter.format(this.$store.getters.totalLiabilities)
-            },
-            netWorth() {
-                return utils.usdFormatter.format(this.$store.getters.netWorth)
-            },
-            sixMonthsExpenses() {
-                return this.$store.getters.averageMandatoryExpenses * 6
-            },
-            surplusDeficit() {
-                return this.$store.getters.totalCash - this.sixMonthsExpenses
-            },
-            amountNeededForFinancialIndependence() {
-                return utils.calculateAmountToCoverExpenses(this.$store.getters.averageMandatoryExpenses)
-            },
-            timeUntilFinancialIndependence() {
-                return utils.compoundInterestTime(this.amountNeededForFinancialIndependence, this.$store.getters.netWorth, 12)
-            }
+        netWorth() {
+            return utils.usdFormatter.format(this.$store.getters.netWorth)
         },
-        methods: {
-            getData() {
-                // get all data needed sequentially, and set "show it" when done.
-                const p = new Promise((resolve, reject) => {
-                    api.getLiabilities().then((data) => {
-                        this.$store.commit('setLiabilities', data)
-                    }).then(() => {
-                        return api.getAssets().then((data) => {
-                            this.$store.commit('setAssets', data)
-                        })
-                    }).then(() => {
-                        return api.getExpenses().then((data) => {
-                            this.$store.commit('setExpenses', data)
-                        })
-                    }).then(() => {
-                        return api.getBudgets().then((data) => {
-                            this.$store.commit('setBudgets', data)
-                        })
-                    }).then(() => {
-                        const now = new Date(Date.now())
-                        const month = now.getMonth() + 1
-                        const year = now.getFullYear()
-                        const budgets = this.$store.getters.budgetByDate(month, year)
-                        if (budgets && budgets.length >= 1) {
-                            const budget = budgets[0]
-                            budget.netWorth = this.$store.getters.netWorth
-                            return api.updateBudget(budget).then(() => {
-                                this.loading = false
-                                resolve()
-                            })
-                        } else {
-                            const p = new Promise((resolve, reject) => {
-                                this.loading = false
-                                resolve()
-                            })
-                            return p.then(() => { resolve() })
-                        }
-                    }).catch((error) => {
-                        this.loading = false
-                        this.error = error
-                        reject()
-                    })
+        sixMonthsExpenses() {
+            return this.$store.getters.averageMandatoryExpenses * 6
+        },
+        surplusDeficit() {
+            return this.$store.getters.totalCash - this.sixMonthsExpenses
+        },
+        amountNeededForFinancialIndependence() {
+            return utils.calculateAmountToCoverExpenses(this.$store.getters.averageMandatoryExpenses)
+        },
+        timeUntilFinancialIndependence() {
+            return utils.compoundInterestTime(
+                this.amountNeededForFinancialIndependence,
+                this.$store.getters.netWorth,
+                12
+            )
+        }
+    },
+    methods: {
+        async getData() {
+            // get all data needed concurrently, and set in store as it is received.
+            const promises = []
+            promises.push(
+                api.getLiabilities().then(data => {
+                    this.$store.commit('setLiabilities', data)
                 })
-                return p
-            },
-            createNetWorthBarChart() {
-                const netWorthBarChartContext = document.getElementById('netWorthBarChart')
-                if (!netWorthBarChartContext) {
-                    return
-                }
-                new Chart(netWorthBarChartContext, {
-                    type: 'bar',
-                    data: {
-                        labels: ['Assets', 'Liabilities', 'Net Worth'],
-                        datasets: [{
-                            data: [this.$store.getters.totalAssets, this.$store.getters.totalLiabilities, this.$store.getters.netWorth],
-                            backgroundColor: [
-                                'rgba(0, 255, 0, 0.2)',
-                                'rgba(255, 0, 0, 0.2)',
-                                'rgba(0, 0, 255, 0.2)'
-                            ]
-                        }]
+            )
+            promises.push(
+                api.getAssets().then(data => {
+                    this.$store.commit('setAssets', data)
+                })
+            )
+            promises.push(
+                api.getExpenses().then(data => {
+                    this.$store.commit('setExpenses', data)
+                })
+            )
+            promises.push(
+                api.getBudgets().then(data => {
+                    this.$store.commit('setBudgets', data)
+                })
+            )
+
+            await Promise.all(promises)
+            const now = new Date(Date.now())
+            const month = now.getMonth() + 1
+            const year = now.getFullYear()
+            const budgets = this.$store.getters.budgetByDate(month, year)
+            if (budgets && budgets.length >= 1) {
+                const budget = budgets[0]
+                budget.netWorth = this.$store.getters.netWorth
+                await api.updateBudget(budget)
+            }
+        },
+        createNetWorthBarChart() {
+            const netWorthBarChartContext = document.getElementById('netWorthBarChart')
+            if (!netWorthBarChartContext) {
+                return
+            }
+            new Chart(netWorthBarChartContext, {
+                type: 'bar',
+                data: {
+                    labels: ['Assets', 'Liabilities', 'Net Worth'],
+                    datasets: [
+                        {
+                            data: [
+                                this.$store.getters.totalAssets,
+                                this.$store.getters.totalLiabilities,
+                                this.$store.getters.netWorth
+                            ],
+                            backgroundColor: ['rgba(0, 255, 0, 0.2)', 'rgba(255, 0, 0, 0.2)', 'rgba(0, 0, 255, 0.2)']
+                        }
+                    ]
+                },
+                options: {
+                    legend: {
+                        display: false
                     },
-                    options: {
-                        legend: {
-                            display: false
-                        },
-                        scales: {
-                            yAxes: [{
+                    scales: {
+                        yAxes: [
+                            {
                                 ticks: {
                                     callback(label, index, labels) {
                                         return utils.usdFormatter.format(label)
                                     }
                                 }
-                            }]
-                        },
-                        tooltips: {
-                            callbacks: {
-                                label(tooltipItem, data) {
-                                    return utils.usdFormatter.format(tooltipItem.yLabel)
-                                }
                             }
-                        }
-                    }
-                })
-            },
-            createIndependenceEstimatePieChart() {
-                const independenceEstimatePieChartContext = document.getElementById('independenceEstimatePieChart')
-                if (!independenceEstimatePieChartContext) {
-                    return
-                }
-                const amountRemaining = Math.max(this.amountNeededForFinancialIndependence - this.$store.getters.netWorth, 0)
-                new Chart(independenceEstimatePieChartContext, {
-                    type: 'pie',
-                    data: {
-                        datasets: [{
-                            data: [this.$store.getters.netWorth, amountRemaining],
-                            backgroundColor: [
-                                'rgba(0, 0, 255, 0.2)',
-                                'rgba(0, 255, 0, 0.2)'
-                            ]
-                        }],
-                        labels: ['Amount Saved', 'Amount Remaining']
+                        ]
                     },
-                    options: {
-                        tooltips: {
-                            callbacks: {
-                                label(tooltipItem, data) {
-                                    const amount = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
-                                    return utils.usdFormatter.format(amount)
-                                }
+                    tooltips: {
+                        callbacks: {
+                            label(tooltipItem, data) {
+                                return utils.usdFormatter.format(tooltipItem.yLabel)
                             }
                         }
                     }
-                })
+                }
+            })
+        },
+        createIndependenceEstimatePieChart() {
+            const independenceEstimatePieChartContext = document.getElementById('independenceEstimatePieChart')
+            if (!independenceEstimatePieChartContext) {
+                return
             }
+            const amountRemaining = Math.max(
+                this.amountNeededForFinancialIndependence - this.$store.getters.netWorth,
+                0
+            )
+            new Chart(independenceEstimatePieChartContext, {
+                type: 'pie',
+                data: {
+                    datasets: [
+                        {
+                            data: [this.$store.getters.netWorth, amountRemaining],
+                            backgroundColor: ['rgba(0, 0, 255, 0.2)', 'rgba(0, 255, 0, 0.2)']
+                        }
+                    ],
+                    labels: ['Amount Saved', 'Amount Remaining']
+                },
+                options: {
+                    tooltips: {
+                        callbacks: {
+                            label(tooltipItem, data) {
+                                const amount = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
+                                return utils.usdFormatter.format(amount)
+                            }
+                        }
+                    }
+                }
+            })
         }
     }
-
+}
 </script>
 
 <style scoped=true>
-    table {
-        width: 100%;
-    }
+table {
+    width: 100%;
+}
 
-    .fi-estimate {
-        font-weight: bold;
-    }
+.fi-estimate {
+    font-weight: bold;
+}
 
-    h3.table-header {
-        margin-top: 0;
-    }
+h3.table-header {
+    margin-top: 0;
+}
 
+.grid-2-container {
+    display: grid;
+    grid-gap: 1em;
+    margin: 1em;
+}
+
+canvas {
+    align-self: center;
+}
+
+@media screen and (min-width: 680px) {
     .grid-2-container {
-        display: grid;
-        grid-gap: 1em;
-        margin: 1em;
+        align-items: center;
+        grid-template-columns: 1fr 1fr;
+        margin: 0;
     }
-
-    canvas {
-        align-self: center;
-    }
-
-    @media screen and (min-width: 680px) {
-        .grid-2-container {
-            align-items: center;
-            grid-template-columns: 1fr 1fr;
-            margin: 0;
-        }
-    }
+}
 </style>
