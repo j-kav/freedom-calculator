42274716d1e90e4cd3e404dbe08510371b02b36f
Scott Anderson <andersco@gmail.com>
2018-05-26 19:23:54 -0700

    Fix tests

--- src/webpack.config-test.babel.js
+++ src/webpack.config-test.babel.js
@@ -1,11 +1,14 @@
 import nodeExternals from 'webpack-node-externals'
+import VueLoaderPlugin from 'vue-loader/lib/plugin'
 import 'babel-polyfill'
 
 export default {
-    target: 'node',
     externals: [nodeExternals()],
+    plugins: [
+        new VueLoaderPlugin()
+    ],
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 loader: 'babel-loader'
@@ -13,6 +16,13 @@ export default {
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
             }
         ]
     }
