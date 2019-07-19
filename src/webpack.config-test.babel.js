be04d10f55dcdb0207cf05ecec861766013b25b9
Scott Anderson <andersco@gmail.com>
2019-07-19 12:07:31 -0700

    Update packages

--- src/webpack.config-test.babel.js
+++ src/webpack.config-test.babel.js
@@ -2,6 +2,11 @@ import nodeExternals from 'webpack-node-externals'
 import VueLoaderPlugin from 'vue-loader/lib/plugin'
 import 'babel-polyfill'
 
+// HACK - this is to fix this bug: https://github.com/vuejs/vue-cli/issues/2128
+// TODO - remove when the bug is fixed or a better solution is found
+import 'jsdom-global'
+window.Date = Date;
+
 export default {
     externals: [nodeExternals()],
     plugins: [
